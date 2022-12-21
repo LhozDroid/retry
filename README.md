0cd84824c48eede6ca90e76bfc974436dff79532
Luis Salas <lhozdroid@gmail.com>
2022-12-21 13:24:36 -0500

    Added example

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Java Retry
 A simple and small framework to retry a method...
 
-1- Use the builder to construct the retry objects
+1- Use the builder to create the retry object
 
 ```java
 final Retry retry = new RetryBuilder()//
@@ -15,11 +15,7 @@ final Retry retry = new RetryBuilder()//
 2- Execute it
 
 ```java
-try {
-	retry.run();
-} catch (final RetryException e) {
-	LOG.error(e.getLocalizedMessage(), e);
-}
+retry.run();
 ```
 
 <a href="https://www.paypal.com/donate/?cmd=_donations&business=CSQRVLE2D43NU&item_name=Buy+me+a+beer!&currency_code=USD">
---
+++ src/test/java/dev/lhoz/resilence/retry/example/RetryExample.java
@@ -0,0 +1,40 @@
+/**
+ *
+ */
+package dev.lhoz.resilence.retry.example;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import dev.lhoz.resilence.retry.Retry;
+import dev.lhoz.resilence.retry.RetryBuilder;
+import dev.lhoz.resilence.retry.RetryException;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author Lhoz
+ *
+ */
+@Log4j2
+public class RetryExample {
+	/**
+	 * @param args
+	 * @throws RetryException
+	 */
+	public static void main(final String[] args) throws RetryException {
+		final AtomicInteger wait = new AtomicInteger(3);
+
+		final Retry retry = new RetryBuilder()//
+				.withAttempts(4)//
+				.withSleep(500)//
+				.withMethod(() -> {
+					if (wait.get() > 0) {
+						RetryExample.LOG.info("Will retry...");
+						wait.set(wait.get() - 1);
+						throw new RuntimeException();
+					}
+					RetryExample.LOG.info("Success");
+				}).build();
+
+		retry.run();
+	}
+}
